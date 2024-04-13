aefbee3643cb750b839c9435a7f097ff6514daa6
Kaio Lima <kaio.lima542@gmail.com>
2024-04-12 23:20:50 -0300

    Initial commit

---
+++ src/Principal.java
@@ -0,0 +1,29 @@
+import br.com.cineSteam.modulos.Filme;
+import br.com.cineSteam.modulos.Serie;
+
+public class Principal {
+    public static void main(String[] args) {
+
+        Filme meuFilme = new Filme();
+        meuFilme.setNome("O podereso chefão");
+        meuFilme.setAnoDeLancamento(1970);
+        meuFilme.setDuracaoEmMinutos(180);
+        System.out.println("Duração do filme: " + meuFilme.getDuracaoEmMinutos());
+
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
+        meuFilme.avalia(10);
+        System.out.println("Total de avalizações: " + meuFilme.getTotalDeAvaliacoes());
+        System.out.println(meuFilme.retornaMedia());
+
+        Serie friends = new Serie();
+        friends.setNome("Friends");
+        friends.setAnoDeLancamento(2000);
+        friends.exibeFichaTecnica();
+        friends.setTemporadas(10);
+        friends.setEspisodiosPorTemporada(10);
+        friends.setMinutosPorEspisodio(50);
+        System.out.println("Duração para assitir: " + friends.getDuracaoEmMinutos());
+    }
+}
