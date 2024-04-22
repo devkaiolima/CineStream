8f9962cd9bbd8cf76524cdb46b62a7a8522c6951
Kaio Lima <kaio.lima542@gmail.com>
2024-04-22 15:58:11 -0300

    adicionando comentarios

--- src/br/com/cinesteam/calculos/CalculadoraDeTempo.java
+++ src/br/com/cinesteam/calculos/CalculadoraDeTempo.java
@@ -1,6 +1,6 @@
 package br.com.cinesteam.calculos;
 
-import br.com.cinesteam.modelos.Filme;
+import br.com.cinesteam.modelos.Titulo;
 
 public class CalculadoraDeTempo {
 
@@ -10,7 +10,15 @@ public class CalculadoraDeTempo {
         return tempoTotal;
     }
 
-    public void inclui(Filme f) {
-        tempoTotal += f.getDuracaoEmMinutos();
+//    public void inclui(Filme f) {
+//        tempoTotal += f.getDuracaoEmMinutos();
+//    }
+//
+//    public void inclui(Serie s) {
+//        tempoTotal += s.getDuracaoEmMinutos();
+//    }
+
+    public void inclui(Titulo titulo) {
+        this.tempoTotal += titulo.getDuracaoEmMinutos();
     }
 }
