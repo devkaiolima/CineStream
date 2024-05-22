19e7caaf31d2ab5e113093c04e05e7b5b686ddb6
Kaio Lima <kaio.lima542@gmail.com>
2024-05-22 16:49:20 -0300

    Transfomando dados com a biblioteca GSON

--- src/br/com/cinesteam/modelos/Titulo.java
+++ src/br/com/cinesteam/modelos/Titulo.java
@@ -1,8 +1,12 @@
 package br.com.cinesteam.modelos;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Titulo implements Comparable<Titulo> {
 
+    @SerializedName("Title")
     private String nome;
+    @SerializedName("Year")
     private int anoDeLancamento;
     private boolean incluidoNoPlano;
     private double somaDasAvaliacoes;
@@ -12,6 +16,12 @@ public class Titulo implements Comparable<Titulo> {
     public Titulo(String nome, int anoDeLancamento) {
         this.nome = nome;
         this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public Titulo(TituloOmdb meuTituloOmdb) {
+        this.nome = meuTituloOmdb.title();
+        this.anoDeLancamento = Integer.valueOf(meuTituloOmdb.year());
+        this.duracaoEmMinutos = Integer.valueOf(meuTituloOmdb.runtime().substring(0,2));
     }
 
     public String getNome() {
@@ -67,5 +77,12 @@ public class Titulo implements Comparable<Titulo> {
     @Override
     public int compareTo(Titulo outroTitulo) {
         return this.getNome().compareTo(outroTitulo.getNome());
+    }
+
+    @Override
+    public String toString() {
+        return "nome='" + nome + '\'' +
+                ", anoDeLancamento=" + anoDeLancamento + ","
+                + "duração=" + duracaoEmMinutos + '}';
     }
 }
--- src/br/com/cinesteam/principal/PrincipalComBusca.java
+++ src/br/com/cinesteam/principal/PrincipalComBusca.java
@@ -1,5 +1,11 @@
 package br.com.cinesteam.principal;
 
+import br.com.cinesteam.modelos.Titulo;
+import br.com.cinesteam.modelos.TituloOmdb;
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -22,6 +28,15 @@ public class PrincipalComBusca {
 
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(response.body());
+        String json = response.body();
+        System.out.println(json);
+
+        Gson gson =  new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
+
+        TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
+        System.out.println(meuTituloOmdb);
+        Titulo meuTitulo = new Titulo(meuTituloOmdb);
+        System.out.println("Titulo convertido");
+        System.out.println(meuTitulo);
     }
 }
